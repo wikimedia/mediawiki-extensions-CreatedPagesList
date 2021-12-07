ed6f10e3f1a13a4b72ab85f6536d1a2c4a03025b
Umherirrender <umherirrender_de.wp@web.de>
2021-12-07 12:41:43 +0100

    Emit deprecation warning from php entry point
    Change-Id: I65d821e8c8b50c49acd7fbc84973a8671fd5e918

--- CreatedPagesList.php
+++ CreatedPagesList.php
@@ -9,6 +9,10 @@
  */
 
 if ( function_exists( 'wfLoadExtension' ) ) {
+	wfWarn(
+		'Deprecated PHP entry point used for CreatedPagesList extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 	wfLoadExtension( 'CreatedPagesList' );
 } else {
 	die( 'This version of the CreatedPagesList extension requires MediaWiki 1.35+' );
